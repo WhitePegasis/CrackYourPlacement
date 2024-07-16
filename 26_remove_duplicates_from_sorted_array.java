82a677a9aa458381239ff6f9a95d63fee504e5d2
Sourav Pradhan <whitepegasus1603@gmail.com>
2024-07-16 23:27:48 +0530

    solved que 1,2,3 and 5

---
+++ 26_remove_duplicates_from_sorted_array.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=1, n=nums.length;
+
+        int count=1;
+
+        while(i<n){
+            while(i<n && nums[i] == nums[i-1]) i++;
+
+            if(i<n){
+                nums[count] = nums[i];
+                count++;
+            }
+            
+            i++;
+        }
+
+        return count;
+    }
+}
